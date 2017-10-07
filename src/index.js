c5c565205bf054d7232d1618cddb336c25678ad7
Jesse Hon <honkalong@gmail.com>
2017-10-07 15:40:30 +1100

    Add missing semicolons

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
 
-import store, { history } from './store'
+import store, { history } from './store';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-const targetElm = document.getElementById('root')
+const targetElm = document.getElementById('root');
 
 render(
   <Provider store={store}>
@@ -17,6 +17,6 @@ render(
     </ConnectedRouter>
   </Provider>,
   targetElm
-)
+);
 
 registerServiceWorker();
