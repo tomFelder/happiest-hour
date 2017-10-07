c74b37f9b36bc4481677b23ad0574ddaa0e10bd8
Jesse Hon <honkalong@gmail.com>
2017-10-07 15:25:01 +1100

    Configure react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+
+import store, { history } from './store'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const targetElm = document.getElementById('root')
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  targetElm
+)
+
 registerServiceWorker();
